dae906d9dbda86f73984fa32d7a5c77e044ce99e
SergioLeiva <sergioleivag@hotmail.es>
2015-05-31 21:35:28 +0200

    Cuarto ejercicio

--- TestJUnit.java
+++ TestJUnit.java
@@ -3,41 +3,55 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import bgfhngfbv.BinaryStringException;
+
 public class TestJUnit {
-  private BinaryString bitset1,bitset2,bitset3,bitset4;
-  private String  bits= "1001001010",bst3="100101", bst4="0110101";
-	
-  //Inicializar todos los bitset
-  @Before
-	public void Iniciar(){
-	bitset1 = new BinaryString(10);
-	bitset2 = new BinaryString(bits);
+	private BinaryString bitset1, bitset2, bitset3, bitset4;
+	private String bits = "1001001010", bst3 = "100101", bst4 = "0110101";
+
+	// Inicializar todos los bitset
+	@Before
+	public void Iniciar() {
+		bitset1 = new BinaryString(10);
+		bitset2 = new BinaryString(bits);
 	}
-	
-	
-	
-	@Test			//Ejercicio 1
+
+	@Test
+	// Ejercicio 1
 	public void testLength() {
-		assertEquals(10,bitset1.length());
-		assertEquals(10,bitset2.length());
+		assertEquals(10, bitset1.length());
+		assertEquals(10, bitset2.length());
 	}
-	
-	
-	@Test			//Ejercicio 2
-	public void testSetBit(){
-		bitset3.set(1,'1');
+
+	@Test
+	// Ejercicio 2
+	public void testSetBit() {
+		bitset3.set(1, '1');
 		assertEquals('1', bitset3.get(1));
 	}
-					//Ejercicio 3
+
+	// Ejercicio 3
 	@Test(expected = BinaryStringException.class)
-	public void testGetOutOfLimitBit(){
+	public void testGetOutOfLimitBit() {
 		bitset4.get(56);
 	}
-					//Ejercicio 4
+
+	// Ejercicio 4
 	@Test(expected = BinaryStringException.class)
-	public void testSetOutOfLimitBit(){
-		bitset4.set(56,'0');
+	public void testSetOutOfLimitBit() {
+		bitset4.set(56, '0');
 	}
-	
+
+	// Ejercicio 5
+	@Test
+	public void testOutOfLimitV2() {
+		try {
+			bitset4.set(56, '0');
+			fail("Se espera excepción BinaryStringException");
+		} catch (BinaryStringException e) {
+
+		}
+
+	}
 
 }
